555a3e364ce0762a9885194f8481ca0099c7884c
Valerie Osawe-edoh <113637565+Goldfishvee@users.noreply.github.com>
2023-02-07 22:16:01 +0100

    Update 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,7 +9,8 @@ def inherits_from(obj, a_class):
     Arg:
         obj (any obj): target object whose inheritance is to be checked.
         a_class (type): class to match obj to.
-    Return:
+        
+        Return:
         True if is inherited from instance of a_class
         False if otherwise
     """
