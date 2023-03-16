da2c356ff3d9426b29946824f736bd39f4a31d0c
Valerie Osawe-edoh <113637565+Goldfishvee@users.noreply.github.com>
2023-03-16 19:10:54 +0100

    Create 103-max_state.sql

---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,7 @@
+-- This script will display max temps of each state in db
+
+-- Results will be ordered by state name
+
+SELECT `state`, MAX(`value`) AS `max_temp` FROM `temperatures`
+
+GROUP BY `state` ORDER BY `state` ASC;
