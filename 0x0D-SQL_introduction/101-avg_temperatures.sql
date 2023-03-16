8354715acdcf20d5159afe0c26c1c448f6e7de12
Valerie Osawe-edoh <113637565+Goldfishvee@users.noreply.github.com>
2023-03-16 19:09:47 +0100

    Create 101-avg_temperatures.sql

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,7 @@
+-- This script will find the avg temp (in deg Fahr) from an imported db
+
+-- sort the avg_temp by city and in descending order
+
+SELECT `city`, AVG(`value`) AS `avg_temp` FROM `temperatures`
+
+GROUP BY `city` ORDER BY `avg_temp` DESC;
