b087419e03af824e2e580b15290ffd3c1dd9cd30
Valerie Osawe-edoh <113637565+Goldfishvee@users.noreply.github.com>
2023-04-12 21:39:15 +0100

    Create 6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+/* This script will print multi-lines of string using loops
+  Use an array of string to store strings
+  Use only one console.log
+  Not allowed to use if/else. Only a loop (while, for, etc)
+*/
+
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
