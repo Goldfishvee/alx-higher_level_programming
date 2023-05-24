9adfec2348a1d8bbd944e7e4084145523921931b
Valerie Osawe-edoh <113637565+Goldfishvee@users.noreply.github.com>
2023-05-24 06:42:17 +0100

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,21 +1,9 @@
 #!/usr/bin/node
-
-// Status Of Get request
-
 const request = require('request');
-
 const url = process.argv[2];
 
-request.get(url, function (error, response) {
-
-  if (error) {
-
-    console.log(error);
-
-  } else {
-
-    console.log('code:' + ' ' + response.statusCode);
-
-  }
-
-});
+request
+  .get(url)
+  .on('response', (response) => {
+    console.log('code: ' + response.statusCode);
+  });
