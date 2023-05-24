0286ae36a5d09b2666798913a65de897f9878fa1
Valerie Osawe-edoh <113637565+Goldfishvee@users.noreply.github.com>
2023-05-24 06:33:44 +0100

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,21 +1,7 @@
 #!/usr/bin/node
+const fs = require('fs');
+const filePath = process.argv[2];
 
-// Read from file
-
-const filesys = require('fs');
-
-filesys.readFile(process.argv[2], 'utf-8',
-
-  function (err, data) {
-
-    if (err) {
-
-      console.log(err);
-
-      return;
-
-    }
-
-    console.log(data);
-
-  });
+fs.readFile(filePath, 'utf-8', (error, data) => {
+  if (error) { console.log(error); } else { console.log(data); }
+});
